877b684a0b4fce1e6f1f7870090cec88c8c67683
Prerna Soni <prernachouksey.10@gmail.com>
2026-09-19 19:09:16 +0530

    Configure Django for Vercel deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = 'django-insecure-=u47$u+8-tl8oa%%4fe106tk8b#!wn^*7_q-rju*ysjy0(q$=$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
