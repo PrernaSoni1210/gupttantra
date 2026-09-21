1fdbf44d33f7f834bc10ceb3eeb2c41d0b47a98f
Prerna Soni <prernachouksey.10@gmail.com>
2026-09-21 18:29:12 +0530

    Configure static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -124,12 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 
 
 # Email
